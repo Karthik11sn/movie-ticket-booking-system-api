185adb73d6f16adb545a66de1dc8420b896a9749
Karthik11sn <karthiksn11.gmail.com>
2025-04-23 11:03:11 +0530

    Input Dto updating Successfully

--- src/main/java/com/example/movie/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/movie/service/serviceImpl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.movie.entity.TheaterOwner;
 import com.example.movie.entity.User;
 import com.example.movie.entity.UserDetails;
 import com.example.movie.exceptions.UserExistByEmailException;
-import com.example.movie.repository.Userrepository;
+import com.example.movie.repository.UserRepository;
 import com.example.movie.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,19 +12,23 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
- private final Userrepository userrepository;
+
+ private final UserRepository userrepository;
 
     @Override
     public UserDetails addUser(UserDetails user) {
-        if (!userrepository.existsByEmail(user.getEmail())){
+        if (!userrepository.existsByEmail(user.getEmail()))
+            throw new UserExistByEmailException("user with the Email is alredy exists");
             //return copy(user);
-            switch (user.getUserRole()){
-                case USER ->copy(new User(),user);
-                case THEATER_OWNER ->copy(new TheaterOwner(),user);
+            UserDetails userDetails = switch (user.getUserRole()) {
+                case USER -> copy(new User(), user);
+                case THEATER_OWNER -> copy(new TheaterOwner(), user);
 
-            }
-        }
-        throw new UserExistByEmailException("user with the Email is alredy exists");
+
+        };
+            System.out.println(user);
+            return userDetails;
+
     }
     private UserDetails copy(UserDetails userRole,UserDetails user){
         //Userdeatials userRole =user.getUserRole()==UserRole.USER? new user()
