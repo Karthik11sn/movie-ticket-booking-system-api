d244183d4db46c1900768087d11f8510289b9c1c
Karthik11sn <karthiksn11.gmail.com>
2025-04-29 12:47:29 +0530

    Reuest rename

--- src/main/java/com/example/movie/controller/TheaterController.java
+++ src/main/java/com/example/movie/controller/TheaterController.java
@@ -10,7 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 public class TheaterController {
@@ -22,11 +22,13 @@ public class TheaterController {
         TheaterResponse theaterResponse = theaterService.addTheater(email, theaterRequest);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been succesfull created", theaterResponse);
     }
+
     @GetMapping("theaters/{theaterId}")
     public ResponseEntity<ResponseStructure<TheaterResponse>> findTheater(@PathVariable String theaterId){
         TheaterResponse theaterResponse = theaterService.findTheater(theaterId);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been sucessfully fetched", theaterResponse);
     }
+
     @PutMapping("/theaters/{theaterId}")
     public ResponseEntity<ResponseStructure<TheaterResponse>> updateTheater(@PathVariable String theaterId, @Valid @RequestBody TheaterRequest registerationRequest){
         TheaterResponse theaterResponse = theaterService.updateTheater(theaterId, registerationRequest);
