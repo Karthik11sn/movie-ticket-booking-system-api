97d1f518a84a0cae982f6ce3ec16dbc6d5438eae
Karthik11sn <karthiksn11.gmail.com>
2025-04-30 15:38:45 +0530

    UserDetails Security Configuration

--- src/main/java/com/example/movie/controller/TheaterController.java
+++ src/main/java/com/example/movie/controller/TheaterController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
@@ -18,6 +19,7 @@ public class TheaterController {
     private final RestResponseBuilder responseBuilder;
 
     @PostMapping("/theaters")
+    @PreAuthorize("hasAuthority('THEATER_OWNER')")
     public ResponseEntity<ResponseStructure<TheaterResponse>> addTheater(String email, @Valid @RequestBody TheaterRequest theaterRequest){
         TheaterResponse theaterResponse = theaterService.addTheater(email, theaterRequest);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been succesfull created", theaterResponse);
@@ -30,8 +32,10 @@ public class TheaterController {
     }
 
     @PutMapping("/theaters/{theaterId}")
+    @PreAuthorize("hasAuthority('THEATER_OWNER')")
     public ResponseEntity<ResponseStructure<TheaterResponse>> updateTheater(@PathVariable String theaterId, @Valid @RequestBody TheaterRequest registerationRequest){
         TheaterResponse theaterResponse = theaterService.updateTheater(theaterId, registerationRequest);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been sucessfully Updated", theaterResponse);
     }
+
 }
--- src/main/java/com/example/movie/repository/UserRepository.java
+++ src/main/java/com/example/movie/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.movie.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDetails,String> {
+
     boolean existsByEmail(String email);
     UserDetails findByEmail(String email);
 }
